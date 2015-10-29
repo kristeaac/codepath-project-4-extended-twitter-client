2152491b1bffc79626918945ac181db63adf9e56
Kristy Caster <kcaster@walmartlabs.com>
2015-10-28 23:06:02 -0700

    remove comment

--- app/src/main/java/com/codepath/apps/twitter/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/twitter/TwitterClient.java
@@ -17,18 +17,6 @@ import com.loopj.android.http.RequestParams;
 import java.io.IOException;
 import java.util.List;
 
-/*
- * 
- * This is the object responsible for communicating with a REST API. 
- * Specify the constants below to change the API being communicated with.
- * See a full list of supported API classes: 
- *   https://github.com/fernandezpablo85/scribe-java/tree/master/src/main/java/org/scribe/builder/api
- * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
- * Add methods for each relevant endpoint in the API.
- * 
- * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
- * 
- */
 public class TwitterClient extends OAuthBaseClient {
 	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
 	public static final String REST_URL = "https://api.twitter.com/1.1";
